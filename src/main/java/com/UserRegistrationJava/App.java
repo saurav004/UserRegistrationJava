52cb61c3e37f1cc561ef337f17b6d2fb02bf06b1
saurav004 <logtosaurav@gmail.com>
2020-11-24 21:20:24 +0530

    Added:Welcome to User Registration Program

---
+++ src/main/java/com/UserRegistrationJava/App.java
@@ -0,0 +1,9 @@
+package com.UserRegistrationJava;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO USER REGISTRATION PROGRAM" );
+    }
+}
